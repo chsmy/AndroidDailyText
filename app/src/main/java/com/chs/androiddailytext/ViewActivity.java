86c062861f0f7e9afb276ad894f0eb9aa31d1847
caoweiaaa <13161083183@163.com>
2017-11-06 11:17:57 +0800

    view

--- app/src/main/java/com/chs/androiddailytext/ViewActivity.java
+++ app/src/main/java/com/chs/androiddailytext/ViewActivity.java
@@ -1,13 +1,39 @@
 package com.chs.androiddailytext;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSeekBar;
+import android.util.Log;
+import android.widget.SeekBar;
+
+import com.chs.androiddailytext.widget.ChangedImageView;
 
 public class ViewActivity extends AppCompatActivity {
-
+    private AppCompatSeekBar mSeekBar;
+    private ChangedImageView mChangedImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+        mChangedImageView = findViewById(R.id.imageview);
+        mSeekBar = findViewById(R.id.seekbar);
+
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mChangedImageView.setCha(progress);
+                Log.i("progress",progress+"");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/chs/androiddailytext/widget/ChangedImageView.java
@@ -0,0 +1,52 @@
+package com.chs.androiddailytext.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+import com.chs.androiddailytext.utils.AppLog;
+
+/**
+ * 作者：chs on 2017-11-06 10:35
+ * 邮箱：657083984@qq.com
+ */
+
+public class ChangedImageView extends android.support.v7.widget.AppCompatImageView {
+
+    private int cha;
+
+    public ChangedImageView(Context context) {
+        super(context);
+    }
+
+    public ChangedImageView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ChangedImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        AppLog.i("onMeasure");
+        int width = getMeasuredWidth();
+        int height = getMeasuredHeight();
+
+        height+=cha;
+        width+=cha;
+
+        setMeasuredDimension(width,height);
+    }
+
+    public void setCha(int cha) {
+        this.cha = cha;
+        requestLayout();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+}
