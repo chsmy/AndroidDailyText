632764876cc95c13510e9f639badae76e5cce85a
caoweiaaa <13161083183@163.com>
2018-04-20 15:07:51 +0800

    统计图

--- app/src/main/java/com/chs/androiddailytext/custom_views/WorkLoadStatisticActivity.java
+++ app/src/main/java/com/chs/androiddailytext/custom_views/WorkLoadStatisticActivity.java
@@ -1,6 +1,7 @@
 package com.chs.androiddailytext.custom_views;
 
 import android.animation.ValueAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -10,8 +11,13 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.chs.androiddailytext.R;
+import com.chs.androiddailytext.module.BarChartEntity;
 import com.chs.androiddailytext.utils.DensityUtil;
+import com.chs.androiddailytext.widget.BarChart2;
 import com.chs.androiddailytext.widget.WorkStatisticView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WorkLoadStatisticActivity extends AppCompatActivity {
     private LinearLayout ll_status;
@@ -36,7 +42,25 @@ public class WorkLoadStatisticActivity extends AppCompatActivity {
                 }
             }
         });
+        initBarChart();
     }
+
+    private void initBarChart() {
+        BarChart2 barChart = findViewById(R.id.bar_chart);
+        List<BarChartEntity> data = new ArrayList<>();
+//        data.add(new BarChartEntity("特种设备组", new Float[]{(float) (Math.random()*1000)}));
+        for(int i =0;i<20;i++){
+//            data.add(new BarChartEntity("第"+String.valueOf(i)+"项", new Float[]{(float) (Math.random()*-1000)}));
+            if(i%2==0){
+                data.add(new BarChartEntity("第"+String.valueOf(i)+"项", new Float[]{(float) (Math.random()*1000)}));
+            }else {
+                data.add(new BarChartEntity("第"+String.valueOf(i)+"项", new Float[]{(float) (Math.random()*-1000)}));
+            }
+        }
+        barChart.setData(data,new int[]{Color.parseColor("#6FC5F4")},"分组","数量",500,-400);
+        barChart.startAnimation();
+    }
+
     private boolean isOpen;
     private void open(final View view){
         ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, DensityUtil.dip2px(this,20));
---
+++ app/src/main/java/com/chs/androiddailytext/widget/PathDivision.java
@@ -0,0 +1,66 @@
+package com.chs.androiddailytext.widget;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.chs.androiddailytext.R;
+import com.chs.androiddailytext.utils.DensityUtil;
+
+/**
+ * 作者：chs
+ * 时间：2018-04-19 17:05
+ * 描述：分割线
+ */
+public class PathDivision extends View {
+    private Paint mPaint;
+    private Path mPath;
+    private int mWidth;
+    public PathDivision(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public PathDivision(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public PathDivision(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+    }
+
+    private void init(Context context) {
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(ContextCompat.getColor(context,R.color.statistic_hand));
+        mPaint.setStrokeWidth(DensityUtil.dip2px(context, 1));
+        mPaint.setStyle(Paint.Style.STROKE);
+
+        mPath = new Path();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        mPath.reset();
+        mPath.moveTo(0,20);
+        mPath.lineTo(mWidth/2-30,20);
+        mPath.lineTo(mWidth/2,0);
+        mPath.lineTo(mWidth/2+30,20);
+        mPath.lineTo(mWidth,20);
+        canvas.drawPath(mPath,mPaint);
+    }
+}
