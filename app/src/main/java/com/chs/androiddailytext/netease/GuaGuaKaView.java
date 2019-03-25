e30b3a78af7f8aa29ed4fe66ca5dec48d20cac0b
caoweiaaa <13161083183@163.com>
2019-03-25 19:44:18 +0800

    刮刮卡

--- app/src/main/java/com/chs/androiddailytext/netease/GuaGuaKaView.java
+++ app/src/main/java/com/chs/androiddailytext/netease/GuaGuaKaView.java
@@ -10,7 +10,6 @@ import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -27,6 +26,7 @@ public class GuaGuaKaView extends View {
     private Path mPath = new Path();
     private float mEventX,mEventY;
     private Canvas mCanvas;
+    private boolean isComplete;
     public GuaGuaKaView(Context context) {
         this(context,null);
     }
@@ -54,9 +54,6 @@ public class GuaGuaKaView extends View {
         mSrcBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.gua);
         mDstBitmap = Bitmap.createBitmap(mSrcBitmap.getWidth(),mSrcBitmap.getHeight(), Bitmap.Config.ARGB_8888);
         mCanvas = new Canvas(mDstBitmap);
-//        mCanvas.drawBitmap(mSrcBitmap,0,0,null);
-//        mDstBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.gua);
-//        mSrcBitmap = Bitmap.createBitmap(mDstBitmap.getWidth(),mDstBitmap.getHeight(), Bitmap.Config.ARGB_8888);
     }
     @Override
     protected void onDraw(Canvas canvas) {
@@ -64,31 +61,27 @@ public class GuaGuaKaView extends View {
         //绘制结果图片
         canvas.drawBitmap(mBitmapRes,0,0,null);
 
-
 //        //第一种 先绘制遮罩层在绘制path,path的画笔使用DST_OUT模式
-//        mCanvas.drawBitmap(mSrcBitmap,0,0,null);
-//        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OUT));
+        mCanvas.drawBitmap(mSrcBitmap,0,0,null);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_OUT));
+        if(!isComplete){
+            mCanvas.drawPath(mPath,mPaint);
+            //绘制目标图像
+            canvas.drawBitmap(mDstBitmap,0,0,null);
+        }
+
+        //第二种//遮罩层绘制在path上面，遮罩层的画笔使用SRC_OUT模式
+        //新建一个图层，不然会把原始图层也当成dst层了。上面的方法之所以不用起一个新图层，因为遮罩层和path都是绘制在新new的canvas中了。
+//        int layerId = canvas.saveLayer(0,0,getWidth(),getHeight(),mPaint,Canvas.ALL_SAVE_FLAG);
+//        //绘制路径
 //        mCanvas.drawPath(mPath,mPaint);
 //        //绘制目标图像
-//        canvas.drawBitmap(mDstBitmap,0,0,null);
-
-          //第二种//遮罩层绘制在path上面，遮罩层的画笔使用SRC_OUT模式
-        //新建一个图层，不然会把原始图层也当成dst层了。上面的方法之所以不用起一个新图层，因为遮罩层和path都是绘制在新new的canvas中了。
-        int layerId = canvas.saveLayer(0,0,getWidth(),getHeight(),mPaint,Canvas.ALL_SAVE_FLAG);
-        //绘制路径
-//        Canvas dstCanvas = new Canvas(mDstBitmap);
-        mCanvas.drawPath(mPath,mPaint);
-        //绘制目标图像
-        canvas.drawBitmap(mDstBitmap,0,0,mPaint);
-        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT));
-
-        canvas.drawBitmap(mSrcBitmap,0,0,mPaint);
-        mPaint.setXfermode(null);
-        canvas.restoreToCount(layerId);
-
-
-
-
+//        canvas.drawBitmap(mDstBitmap,0,0,mPaint);
+//        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT));
+//
+//        canvas.drawBitmap(mSrcBitmap,0,0,mPaint);
+//        mPaint.setXfermode(null);
+//        canvas.restoreToCount(layerId);
     }
 
     @Override
@@ -99,7 +92,6 @@ public class GuaGuaKaView extends View {
                 mEventX = event.getX();
                 mEventY = event.getY();
                 mPath.moveTo(mEventX,mEventY);
-                Log.e("TAG","mEventX"+mEventX+"--mEventY"+mEventY);
                 break;
             case MotionEvent.ACTION_MOVE:
                 float endX = (mEventX+event.getX())/2;
@@ -108,10 +100,65 @@ public class GuaGuaKaView extends View {
 //                mPath.lineTo(event.getX(),event.getY());
                 mEventX = event.getX();
                 mEventY = event.getY();
-                Log.e("TAG------","mEventX"+mEventX+"--mEventY"+mEventY);
                 invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                new Thread(mRunnable).start();
                 break;
         }
         return true;
     }
+
+    /**
+     * 统计擦除区域任务
+     */
+    private Runnable mRunnable = new Runnable()
+    {
+        private int[] mPixels;
+        @Override
+        public void run()
+        {
+            int w = mDstBitmap.getWidth();
+            int h = mDstBitmap.getHeight();
+
+            float wipeArea = 0;
+            float totalArea = w * h;
+
+            Bitmap bitmap = mDstBitmap;
+
+            mPixels = new int[w * h];
+
+            /**
+             * 拿到所有的像素信息
+             */
+            bitmap.getPixels(mPixels, 0, w, 0, 0, w, h);
+
+            /**
+             * 遍历统计擦除的区域
+             */
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    int index = i + j * w;
+                    if (mPixels[index] == 0)
+                    {
+                        wipeArea++;
+                    }
+                }
+            }
+            /**
+             * 根据所占百分比，进行一些操作
+             */
+            if (wipeArea > 0 && totalArea > 0)
+            {
+                int percent = (int) (wipeArea * 100 / totalArea);
+                if (percent > 50)
+                {
+                    isComplete = true;
+                    postInvalidate();
+                }
+            }
+        }
+    };
 }
