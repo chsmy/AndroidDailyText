173919c0fc8bfeaaae2bdbe2dccdbe23c4bf6515
大海 <657083984@qq.com>
2019-04-15 23:00:14 +0800

    粒子效果

--- app/src/main/java/com/chs/androiddailytext/jetpack/LifeActivity.java
+++ app/src/main/java/com/chs/androiddailytext/jetpack/LifeActivity.java
@@ -1,9 +1,18 @@
 package com.chs.androiddailytext.jetpack;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.chs.androiddailytext.R;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.arch.core.util.Function;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.Transformations;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProviders;
 
 /**
  * @author chs
@@ -11,9 +20,28 @@ import androidx.appcompat.app.AppCompatActivity;
  * des：
  */
 public class LifeActivity extends AppCompatActivity {
+    private NameViewModel1 mViewModel;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getLifecycle().addObserver(new MyListener());
+        setContentView(R.layout.activity_name);
+//        getLifecycle().addObserver(new MyListener());
+
+        final TextView textView = findViewById(R.id.tv_name);
+        mViewModel = ViewModelProviders.of(this).get(NameViewModel1.class);
+
+        LiveData<String> userNameMap = Transformations.map(mViewModel.getCurrentName(), new Function<String, String>() {
+            @Override
+            public String apply(String input) {
+                return input + "哈哈哈";
+            }
+        });
+
+        userNameMap.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                textView.setText(s);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/chs/androiddailytext/jetpack/NameViewModel1.java
@@ -0,0 +1,40 @@
+package com.chs.androiddailytext.jetpack;
+
+import android.os.Handler;
+
+import com.google.gson.Gson;
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.callback.StringCallback;
+import com.lzy.okgo.model.Response;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+/**
+ * @author：chs date：2019/4/15
+ * des：
+ */
+public class NameViewModel1 extends ViewModel {
+
+    private MutableLiveData<String> currentName;
+
+    public MutableLiveData<String> getCurrentName() {
+        if (currentName == null) {
+            currentName = new MutableLiveData<String>();
+            loadData();
+        }
+        return currentName;
+    }
+
+    private void loadData() {
+        OkGo.<String>get("http://gank.io/api/xiandu/categories")
+                .execute(new StringCallback() {
+                    @Override
+                    public void onSuccess(Response<String> response) {
+                        Gson gson = new Gson();
+                        Catefories catefories = gson.fromJson(response.body(), Catefories.class);
+                        currentName.postValue(catefories.getResults().get(0).getName());
+                    }
+                });
+    }
+}
